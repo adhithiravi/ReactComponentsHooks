482053a5cb2f5e8df293765fb27d8d6294941e3b
Adhithi Ravichandran <adhithiravi@gmail.com>
2020-03-08 12:00:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,74 +1,24 @@
-import React, { useReducer } from "react";
+import React from "react";
+import ContextComponent from "./ComponentUseContext";
+import ReducerComponent from "./ComponentUseReducer.js";
 
-const initialState = { react: false, graphQL: false, angular: false };
-
-const coursesReducer = (state, action) => {
-  switch (action.type) {
-    case "REACT":
-      return { react: true };
-    case "GRAPHQL":
-      return { graphQL: true };
-    case "ANGULAR":
-      return { angular: true };
-
-    default:
-      throw new Error();
-  }
+const renderReducerHook = () => {
+  console.log("Reducer hook pressed");
+  return <ReducerComponent />;
 };
 
-const Courses = () => {
-  const [state, dispatch] = useReducer(coursesReducer, initialState);
+const renderContextHook = () => {
+  console.log("Context hook pressed");
+  return <ContextComponent />;
+};
 
-  const handleReactPress = () => {
-    dispatch({ type: "REACT" });
-  };
-
-  const handleGraphQLPress = () => {
-    dispatch({ type: "GRAPHQL" });
-  };
-
-  const handleAngularPress = () => {
-    dispatch({ type: "ANGULAR" });
-  };
-
+const App = () => {
   return (
     <div className="box">
-      <h2>Use Reducer Example Component</h2>
-      <p>
-        Learning{" "}
-        {state.react
-          ? "React"
-          : state.graphQL
-          ? "GraphQL"
-          : state.angular
-          ? "Angular"
-          : "Not Started"}
-      </p>
-      <div>
-        <button
-          type="button"
-          onClick={handleReactPress}
-          className="button is-grey"
-        >
-          React
-        </button>
-        <button
-          type="button"
-          onClick={handleGraphQLPress}
-          className="button is-dark"
-        >
-          GraphQL
-        </button>
-        <button
-          type="button"
-          onClick={handleAngularPress}
-          className="button is-grey"
-        >
-          Angular
-        </button>
-      </div>
+      {renderReducerHook()}
+      {renderContextHook()}
     </div>
   );
 };
 
-export default Courses;
+export default App;
